b9f091e6afa4e3167011b76d3c9c9e83aae47429
LorisonGilles <54437398+Gilles30@users.noreply.github.com>
2020-09-26 14:12:58 -0300

    concluido

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,73 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import Order from '../infra/typeorm/entities/Order';
+import IOrdersRepository from '../repositories/IOrdersRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+@injectable()
+class CreateProductService {
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const findProducts = await this.productsRepository.findAllById(products);
+
+    if (findProducts.length !== products.length) {
+      throw new AppError('Products not found', 400);
+    }
+
+    const orderProducts = findProducts.map(findProduct => {
+      const product = products.find(
+        productFilter => productFilter.id === findProduct.id,
+      );
+
+      if (!product || product.quantity > findProduct.quantity) {
+        throw new AppError('Products with insufficient quantities');
+      }
+
+      return {
+        product_id: findProduct.id,
+        price: findProduct.price,
+        quantity: product.quantity,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
+  }
+}
+
+export default CreateProductService;
---
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,71 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+import IUpdateProductsQuantityDTO from '@modules/products/dtos/IUpdateProductsQuantityDTO';
+import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
+
+class ProductsRepository implements IProductsRepository {
+  private ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = getRepository(Product);
+  }
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const findName = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return findName;
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProductDTO): Promise<Product> {
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  public async findAllById(products: IFindProducts[]): Promise<Product[]> {
+    const findProducts = await this.ormRepository.findByIds(products);
+
+    return findProducts;
+  }
+
+  public async updateQuantity(
+    products: IUpdateProductsQuantityDTO[],
+  ): Promise<Product[]> {
+    const updateProducts = products.map(product => {
+      this.ormRepository
+        .createQueryBuilder()
+        .update(Product)
+        .set({ quantity: () => `quantity - ${product.quantity}` })
+        .where('id = :id', { id: `${product.id}` })
+        .execute();
+
+      return product;
+    });
+
+    const findProducts = await this.findAllById(updateProducts);
+
+    return findProducts;
+  }
+}
+
+export default ProductsRepository;
